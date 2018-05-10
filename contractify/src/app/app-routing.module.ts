13f87adb129a623f453076d87f75cba0bf8399d1
Zin Tun <zitun@cisco.com>
2018-05-10 12:11:11 -0700

    Initial routing setup

--- contractify/src/app/app-routing.module.ts
+++ contractify/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+
+
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { FarmownerComponent } from './farmowner/farmowner.component';
+import { TeacherComponent } from './teacher/teacher.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+  ,{path: 'admin', component: AdminComponent}
+  ,{path: 'teacher', component: TeacherComponent}
+  ,{path: 'farmowner', component: FarmownerComponent}
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }
--- contractify/src/app/app.module.ts
+++ contractify/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { TeacherComponent } from './teacher/teacher.component';
 import { FarmownerComponent } from './farmowner/farmowner.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     TeacherComponent,
-    FarmownerComponent
+    FarmownerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
