26c4e8b64a11d21acaedff678135df2e43cbb543
Jimmy Pan <jimpan@cisco.com>
2018-05-10 19:04:10 -0700

    added new stuff to profiles

--- contractify/src/app/teacher/studentprofile/studentprofile.component.ts
+++ contractify/src/app/teacher/studentprofile/studentprofile.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./studentprofile.component.css']
 })
 export class StudentprofileComponent implements OnInit {
-
+  public people = [{
+    name:'Jimmy',
+    numberOfdaysAttended: 0,
+    publicKey:''
+  },{
+    name:'Zin',
+    numberOfdaysAttended: 0,
+    publicKey:''
+  },{
+    name:'Matt',
+    numberOfdaysAttended: 0,
+    publicKey:''
+  },
+  {
+    name:'Ted',
+    numberOfdaysAttended: 0,
+    publicKey:''
+  },
+  {
+    name:'John',
+    numberOfdaysAttended: 0,
+    publicKey:''
+  }
+  ]
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  addToAttendance(index){
+    this.people[index].numberOfdaysAttended ++;
+
   }
 
 }
