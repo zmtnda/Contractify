5f410a8169c9e50cf9e28978b49727def8524ef4
Zin Tun <zitun@cisco.com>
2018-05-10 20:38:55 -0700

    i dont know

--- contractify/src/app/teacher/dashboard/dashboard.component.ts
+++ contractify/src/app/teacher/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+
 })
 export class DashboardComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  chartOptions = {
+    responsive: true
+  };
+
+  chartData = [
+    { data: [30, 40, 50, 60, 70, 80, 90, 100], label: 'Completed' },
+    { data: [20, 30, 40, 50, 60, 70, 80, 90], label: 'Failed' },
+  ];
+  doChartData = [
+    { data: [2, 18, 3]},
+  ];
+
+  chartLabels = ['Sept 2017', 'Oct 2017', 'Nov 2017', 'Dec 2017', 'Jan 2018', 'Feb 2018', 'Mar 2018', 'Apr 2018'];
+  doChartLabels = ['Completed', 'On Track', 'At Risk'];
+
+  onChartClick(event) {
+    console.log(event);
   }
 
 }
--- contractify/src/app/teacher/teacher.module.ts
+++ contractify/src/app/teacher/teacher.module.ts
@@ -6,11 +6,13 @@ import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherComponent } from './teacher.component';
 import { StudentprofileComponent } from './studentprofile/studentprofile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
-    TeacherRoutingModule
+    TeacherRoutingModule,
+    ChartsModule
   ],
   declarations: [
     TeacherComponent,
